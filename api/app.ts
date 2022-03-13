b1043f770aee1e162649a6d06c00ae266fe5c1bb
Robert Kolsek <kolsek.income@gmail.com>
2022-03-13 22:42:33 +0800

    security: add sql-injection package/middleware

--- api/app.ts
+++ api/app.ts
@@ -4,6 +4,7 @@ import * as bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
 import { InversifyExpressServer } from 'inversify-express-utils';
+import sqlinjection from 'sql-injection';
 import * as swagger from 'swagger-express-ts';
 
 import { createSequelizeInstance } from './config/sequelize';
@@ -18,6 +19,7 @@ SequelizeService.start(sequelize);
 const server = new InversifyExpressServer(container);
 
 server.setConfig((app) => {
+  app.use(sqlinjection); // tests included in package https://github.com/ghafran/sql-injection/blob/master/test/index.js
   app.use(cors());
   app.use(bodyParser.json());
   app.use('/api-docs/swagger', express.static('swagger'));
